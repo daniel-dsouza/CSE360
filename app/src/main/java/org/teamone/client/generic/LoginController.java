fb1f3b8306c214f658d17f984e9165f69cdec438
Daniel D'Souza <daniel.dsouza5@gmail.com>
2015-10-06 00:24:45 -0700

    added basic login functionality

--- app/src/main/java/org/teamone/client/generic/LoginController.java
+++ app/src/main/java/org/teamone/client/generic/LoginController.java
@@ -6,10 +6,47 @@ package org.teamone.client.generic;
 
 //import Spring libs
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @Controller
+@RequestMapping(value = "/login")
 public class LoginController {
 
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewLogin(Map<String, Object> model) {
+        User userInput = new User();
+        model.put("userInput", userInput);
+        System.out.print("hi");
+        return "login";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void processLogin(
+            @ModelAttribute("userInput") User user,
+            Map<String, Object> model) {
+        System.out.println(user.username);
+    }
+
+}
+
+class User {
+    public String username;
+    public String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return password;
+    }
 }
