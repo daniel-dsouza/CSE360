6d1bc3b18641d00c6c467e1dbfdafa696840132c
James <jkhutch3@asu.edu>
2015-10-08 16:03:15 -0700

    Their is very simple logic (checks if any of the fields are empty)
    Added the remaining fields that need to be entered
    
    SSN
    Age
    etc.

--- app/src/main/java/org/teamone/client/registration/RegistrationController.java
+++ app/src/main/java/org/teamone/client/registration/RegistrationController.java
@@ -31,7 +31,8 @@ public class RegistrationController {
     public String processLogin(
             @ModelAttribute("userInput") RegistrationAttempt attempt,
             Map<String, Object> model) {
-        /*System.out.println("First Name: " + attempt.getFirstName());
+        /*
+        System.out.println("First Name: " + attempt.getFirstName());
         System.out.println("Last Name: " + attempt.getLastName());
         System.out.println("Date of Birth: " + attempt.getdob());
         System.out.println("Address: " + attempt.getAddress());
@@ -43,8 +44,9 @@ public class RegistrationController {
         System.out.println("Insurance: " + attempt.getInsurance());
         System.out.println("Social Security #: " + attempt.getssn());
         System.out.println("Age: " + attempt.getAge());
-        System.out.println("Gender: " + attempt.getGender());*/
-        if(attempt.getFirstName()==null || attempt.getLastName()==null || attempt.getdob()==null || attempt.getAddress()==null || attempt.getCity()==null || attempt.getState()==null || attempt.getZipcode()==null || attempt.getHomePhone()==null || attempt.getEmail()==null || attempt.getInsurance()==null || attempt.getssn()==null || attempt.getAge()==null || attempt.getGender()==null){
+        System.out.println("Gender: " + attempt.getGender());
+        */
+        if(attempt.getFirstName().equalsIgnoreCase("") || attempt.getLastName().equalsIgnoreCase("") || attempt.getdob().equalsIgnoreCase("") || attempt.getAddress().equalsIgnoreCase("") || attempt.getCity().equalsIgnoreCase("") || attempt.getState().equalsIgnoreCase("") || attempt.getZipcode().equalsIgnoreCase("") || attempt.getHomePhone().equalsIgnoreCase("") || attempt.getEmail().equalsIgnoreCase("") || attempt.getInsurance().equalsIgnoreCase("") || attempt.getssn().equalsIgnoreCase("") || attempt.getAge().equalsIgnoreCase("") || attempt.getGender().equalsIgnoreCase("")){
             //If feild is empty error
             System.out.println("All fields must be filled in");
         }else{
@@ -72,44 +74,44 @@ class RegistrationAttempt {
     private String gender;
 
 
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public String getFirstName() {return this.firstName;}
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getFirstName() { return this.firstName; }
 
-    public void setLastName(String lastName) {this.lastName = lastName;}
-    public String getLastName() {return this.lastName;}
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getLastName() { return this.lastName; }
 
-    public void setdob(String dob) {this.dob = dob;}
-    public String getdob() {return this.dob;}
+    public void setdob(String dob) { this.dob = dob; }
+    public String getdob() { return this.dob; }
 
-    public void setAddress(String address) {this.address = address;}
-    public String getAddress() {return this.address;}
+    public void setAddress(String address) { this.address = address; }
+    public String getAddress() { return this.address; }
 
-    public void setCity(String city) {this.city = city;}
-    public String getCity() {return this.city;}
+    public void setCity(String city) { this.city = city; }
+    public String getCity() { return this.city; }
 
-    public void setState(String state) {this.state = state;}
-    public String getState() {return this.state;}
+    public void setState(String state) { this.state = state; }
+    public String getState() { return this.state; }
 
-    public void setZipcode(String zipcode) {this.zipcode = zipcode;}
-    public String getZipcode() {return this.zipcode;}
+    public void setZipcode(String zipcode) { this.zipcode = zipcode; }
+    public String getZipcode() { return this.zipcode; }
 
-    public void setHomePhone(String homePhone) {this.homePhone = homePhone;}
-    public String getHomePhone() {return this.homePhone;}
+    public void setHomePhone(String homePhone) { this.homePhone = homePhone; }
+    public String getHomePhone() { return this.homePhone; }
 
-    public void setEmail(String email) {this.email = email;}
-    public String getEmail() {return this.email;}
+    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { return this.email; }
 
-    public void setInsurance(String insurance) {this.insurance = insurance;}
-    public String getInsurance() {return this.insurance;}
+    public void setInsurance(String insurance) { this.insurance = insurance; }
+    public String getInsurance() { return this.insurance; }
 
-    public void setssn(String ssn) {this.ssn = ssn;}
-    public String getssn() {return this.ssn;}
+    public void setssn(String ssn) { this.ssn = ssn; }
+    public String getssn() { return this.ssn; }
 
-    public void setAge(String age) {this.age = age;}
-    public String getAge() {return this.age;}
+    public void setAge(String age) { this.age = age; }
+    public String getAge() { return this.age; }
 
-    public void setGender(String gender) {this.gender = gender;}
-    public String getGender() {return this.gender;}
+    public void setGender(String gender) { this.gender = gender; }
+    public String getGender() { return this.gender; }
 
     RegistrationAttempt(){}
 }
