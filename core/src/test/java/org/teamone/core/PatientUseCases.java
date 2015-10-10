500788dc82f62ee27419e4ff0370371b3cba5002
haisheng-lin <hlin86@asu.edu>
2015-10-09 17:02:12 -0700

    we can now update patient in database

--- core/src/test/java/org/teamone/core/PatientUseCases.java
+++ core/src/test/java/org/teamone/core/PatientUseCases.java
@@ -4,31 +4,38 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.teamone.core.Patient;
-import org.teamone.core.PatientUpdateInfo;
+import org.teamone.core.SQL.PatientUpdateInfo;
 
 public class PatientUseCases {
     //this is for all use cases of patient: such as send alert/update info/schedule appointment.....
-    private Patient patient;
+    private Patient test;
 
     @Before
     public void setUp()
     {
-        patient = new Patient();
-        patient.setAddress("Tempe");
-        patient.setPatientID(20151009);
-        patient.setAge(18);
-        patient.setGender("male");
-        patient.setSSN(123456789);
-        patient.setPhone(654321);
-        patient.setOccupation("student");
-        patient.setMedicalHistory("No disease before.");
+        test = new Patient();
+        test.setPatientID(102);
+        test.setfName("Lin");
+        test.setlName("Haisheng");
+        test.setAddress("Tempe");
+        test.setEmail("xxxxxxxx@asu.edu");
+        test.setPhone(1234567890);
+        test.setSSN(123456789);
+        test.setInsurance("nope");
+        test.setAge(18);
+        test.setGender("male");
     }
 
     @Test
     public void tester()
     {
-        int x = PatientUpdateInfo.UpdatePersonalInfo(patient);
-        System.out.print(x);
+        if(PatientUpdateInfo.UpdatePersonalInfo(test))
+        {
+            System.out.println("*********************Update successful************************");
+        }
+        else
+            System.out.println("\n************************Update failed********************");
+        //Person p1 = SQL.cehck
     }
 
 }
