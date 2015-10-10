aee03cd966db4b6cf13b411ebb3b7852264b29ab
jaime <jaimeromerojr94@gmail.com>
2015-10-09 19:58:59 -0700

    Created AppointmentViewEdit.

---
+++ core/src/main/java/org/teamone/core/SQL/AppointmentViewEdit.java
@@ -0,0 +1,18 @@
+package org.teamone.core.SQL;
+
+/**
+ * Created by Jaime on 10/9/2015.
+ * This class will be used to create,view, and edit appointments by both Patients and Staff
+ */
+import org.teamone.core.Patient;
+import org.teamone.core.Staff;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AppointmentViewEdit {
+}
