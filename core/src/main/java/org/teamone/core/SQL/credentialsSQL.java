7c809aabdd3b2564b660b25185008eb60f62305c
rang1 <rang1@asu.edu>
2015-10-20 20:18:44 -0700

    Update credentialsSQL.java

--- core/src/main/java/org/teamone/core/SQL/credentialsSQL.java
+++ core/src/main/java/org/teamone/core/SQL/credentialsSQL.java
@@ -4,12 +4,12 @@ package org.teamone.core.SQL;
  * Created by Ryan on 10/13/2015.
  */
 public class credentialsSQL {
-    public static String remoteMySQLLocation = "jdbc:mysql://localhost:3306/cse360";
-    public static String remoteMySQLuser ="root";
-    public static String remoteMySQLpass = "cse360";
+    //public static String remoteMySQLLocation = "jdbc:mysql://localhost:3306/cse360";//local
+    //public static String remoteMySQLuser ="root";//local
+    //public static String remoteMySQLpass = "cse360";//local
 
-    //public static String remoteMySQLLocation = "jdbc:mysql://sql3.freesqldatabase.com:3306/sql393597";
-    //public static String remoteMySQLuser ="sql393597";
-    //public static String remoteMySQLpass = "eL9*hS6!";
+    public static String remoteMySQLLocation = "jdbc:mysql://sql3.freesqldatabase.com:3306/sql393597";
+    public static String remoteMySQLuser ="sql393597";
+    public static String remoteMySQLpass = "eL9*hS6!";
 
 }
