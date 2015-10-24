ce2c31053ddc748a1e6c642d4b15f1f4bc976470
vinnybod <vrose04@gmail.com>
2015-10-23 17:54:47 -0700

    View Patients Controller

---
+++ app/src/main/java/org/teamone/client/appointments/ViewPatients.java
@@ -0,0 +1,36 @@
+package org.teamone.client.appointments;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.teamone.core.SQL.PatientSQL;
+import org.teamone.core.appointments.Appointment;
+import org.teamone.core.users.Patient;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+//import org.teamone.client.generic.User;
+
+
+@Controller
+@Scope("request")
+@RequestMapping(value = "/ViewPatients") //request mapping usually needed. this will suffice if unless you have multi-page form logic, eg. greater than 2 requests.
+public class ViewPatients {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewUserHome(Map<String, Object> model) {
+        //this is an example of a model attribute
+        ArrayList<Patient> patientList = new ArrayList<Patient>();
+        patientList = PatientSQL.getPatientList();
+
+
+        model.put("patientList", patientList);
+
+        System.out.println(model); //debug statement
+        return "appointment/ViewAppt"; //return the view with linked model
+
+}
