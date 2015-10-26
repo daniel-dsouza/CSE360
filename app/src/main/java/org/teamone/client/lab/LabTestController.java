a2d0797804dcd97a19ac10072d35b8fa6e6816a0
Daniel D'Souza <daniel.dsouza5@gmail.com>
2015-10-26 09:31:28 -0700

    added capability to view lab reports, fixed corresponding navigation links

--- app/src/main/java/org/teamone/client/lab/LabTestController.java
+++ app/src/main/java/org/teamone/client/lab/LabTestController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.teamone.client.generic.User;
+import org.teamone.core.SQL.LabTestSQL;
+import org.teamone.core.users.LabStaff;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,49 +22,53 @@ import java.util.Map;
 @RequestMapping(value="/**/lab_report")
 public class LabTestController {
 
-    @RequestMapping(value="/{id}/view", method= RequestMethod.GET)
+    @RequestMapping(method= RequestMethod.GET)
+    public String list (Map<String, Object> model,
+                        @ModelAttribute User user) {
+        if (user.getPerson() == null)
+            return "redirect:/login";
+        else if (!(user.getPerson() instanceof LabStaff))
+            return "redirect:/user/" + user.person.getUserID();
+        List labReportsList = LabTestSQL.getAllLabTests();
+        model.put("list", labReportsList);
+        return "lab/listlabreport";
+    }
+
+    @RequestMapping(value="/{reportID}/view", method= RequestMethod.GET)
     public String viewGet (Map<String, Object> model,
                         @PathVariable String reportID,
-                        @ModelAttribute User user) {
+                        @ModelAttribute("user") User user) {
         if (user.getPerson() == null)
             return "login/login";
 
         return "lab/viewlabreport";
     }
 
-    @RequestMapping(value="/{id}/edit", method= RequestMethod.GET)
+    @RequestMapping(value="/{reportID}/edit", method= RequestMethod.GET)
     public String editGet (Map<String, Object> model,
                         @PathVariable String reportID,
-                        @ModelAttribute User user) {
+                        @ModelAttribute("user") User user) {
         return "lab/editlabreport";
     }
 
-    @RequestMapping(value="/{id}/edit", method= RequestMethod.POST)
+    @RequestMapping(value="/{reportID}/edit", method= RequestMethod.POST)
     public String editPost (Map<String, Object> model,
                             @PathVariable String reportID,
-                            @ModelAttribute User user) {
+                            @ModelAttribute("user") User user) {
         return "lab/editlabreport";
     }
 
-    @RequestMapping(value="/{id}/create", method= RequestMethod.GET)
+    @RequestMapping(value="/{reportID}/create", method= RequestMethod.GET)
     public String createGet (Map<String, Object> model,
                         @PathVariable String reportID,
-                        @ModelAttribute User user) {
+                        @ModelAttribute("user") User user) {
         return "lab/editlabreport";
     }
 
-    @RequestMapping(value="/{id}/create", method= RequestMethod.POST)
+    @RequestMapping(value="/{reportID}/create", method= RequestMethod.POST)
     public String createPost (Map<String, Object> model,
                              @PathVariable String reportID,
-                             @ModelAttribute User user) {
+                             @ModelAttribute("user") User user) {
         return "lab/editlabreport";
-    }
-
-    @RequestMapping(method= RequestMethod.GET)
-    public String list (Map<String, Object> model,
-                        @PathVariable String reportID,
-                        @ModelAttribute User user) {
-
-        return "lab/listlabreport";
     }
 }
