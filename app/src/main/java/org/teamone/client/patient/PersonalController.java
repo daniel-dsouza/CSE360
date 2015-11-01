7f5323248c08b8c3512bb7a1cdc3750a166f8537
James <jkhutch3@asu.edu>
2015-10-31 17:58:46 -0700

    Modified .java files to properly use user variable: HealthConditionsController.java MedicalHistoryController.java PersonalController.java RegistrationController.java
    Modified SelectPatientController.java so buttons redirected to the correct page.
    
    Modified patient-PersonalInformation.jsp so that it says "Update" rather than "Next" on its button
    
    Added TODOs because I did not know how to add buttons to the patient display.

--- app/src/main/java/org/teamone/client/patient/PersonalController.java
+++ app/src/main/java/org/teamone/client/patient/PersonalController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.teamone.client.generic.User;
 import org.teamone.core.SQL.PatientSQL;
+import org.teamone.core.users.LabStaff;
 import org.teamone.core.users.Patient;
 import org.teamone.core.users.PatientInformation;
 
@@ -16,20 +17,32 @@ import java.util.Map;
  * Created by James on 10/9/2015.
  */
 @Controller
-@RequestMapping(value = "/user/{userid}/personal")
+@RequestMapping(value = "/**/personal")
 public class PersonalController {
 
-    Patient patient = new Patient();
 
+    //TODO add a button so patients can access this function
     @RequestMapping(method = RequestMethod.GET)
     public String editPersonalInformation(Map<String, Object> model,
-                                          @PathVariable int userid) {
+                                          @ModelAttribute("user") User user) {
 
-        patient.setUserID(userid);
-        patient = PatientSQL.getPatientComplete(patient);
-        System.out.println(patient.getAddress());
-        //PersonalUpdate attempt = populatePersonalInformation();
-        PatientInformation attempt = patient.getPatientInformation();
+        if (user.getPerson() == null)
+            return "redirect:/login";
+        else if(user.getPerson() instanceof LabStaff)
+            return "redirect:/user/" + user.person.getUserID();
+        else if(user.getPerson() instanceof Patient){
+            Patient p = new Patient();
+            p.setUserID(user.getPerson().getUserID());
+            user.setPatient(PatientSQL.getPatientComplete(p));
+        }
+
+        if(user.getPatient() == null)               // If patient has not been initialized send them to patient select
+            return "redirect:/select_patient";
+
+
+
+
+        PatientInformation attempt = user.getPatient().getPatientInformation();
         model.put("userInput", attempt);
 
         return "/patient/patient-PersonalInformation";
@@ -37,14 +50,13 @@ public class PersonalController {
 
     @RequestMapping(method = RequestMethod.POST)
     public String processPatientInformation(@ModelAttribute("userInput") PatientInformation attempt,
-                                            @ModelAttribute("user") User user,
-                                            Map<String, Object> model) {
+                                            @ModelAttribute("user") User user) {
 
-        System.out.println("Creating a Patient");
+        //System.out.println("Creating a Patient");
 
         //attempt.displayPersonalInformation();
-        patient.setPatientInformation(attempt);
-        PatientSQL.UpdatePersonalInfo(patient);
+        user.getPatient().setPatientInformation(attempt);
+        PatientSQL.UpdatePersonalInfo(user.getPatient());
 
         //return "/patient/patient-PersonalInformation"; // should be patient's personal information view page
         return "redirect:/user/" + user.person.getUserID();
