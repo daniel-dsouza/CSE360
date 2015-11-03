abdd8f8ec91f53ba0aeec25b8f54522a504f90ec
rang1 <rang1@asu.edu>
2015-11-02 22:24:34 -0700

    fixed vulnerability

--- app/src/main/webapp/js/datepaginator.js
+++ app/src/main/webapp/js/datepaginator.js
@@ -6,24 +6,30 @@
  http://jondmiles.com/bootstrap-datepaginator/#usage
  */
 
+var selected;//globals suck
+$(document).ready(function () {
 
-$(document).ready(function() {
+        var d = startDate();
 
-    var options = {
-        selectedDateFormat: "YYYY-MM-DD",
-        onSelectedDateChanged: function(event, date) {
-            document.getElementById('day').value=formatDate(date);
+        var options = {
+            startDate: d,//first date to be picked. User can not select the past
+            startDateFormat: "YYYY-MM-DD",
+            selectedDate: d,//first date to be selected by default
+            selectedDateFormat: "YYYY-MM-DD",
+            showCalendar: false,
+            onSelectedDateChanged: function (event, date) {
+                selected = formatDate(date);
+            }
         }
+        $('#paginator').datepaginator(options);
+
+        /*$('#paginator').on('selectedDateChanged', function(event, date) {
+         document.getElementById('day').value=formatDate(date); //This way is a vulnerability. If you inspect element,
+                                                           //you can modify the text area. Inject symbols and our program will crash
+         }); */
+
     }
-    $('#paginator').datepaginator(options);
-
-    /* $('#paginator').on('selectedDateChanged', function(event, date) {
-        document.getElementById('day').value=date;
-    });
-    */
-
-
-});
+);
 //http://stackoverflow.com/questions/23593052/format-javascript-date-to-yyyy-mm-dd
 function formatDate(date) {
     var d = new Date(date),
@@ -35,4 +41,30 @@ function formatDate(date) {
     if (day.length < 2) day = '0' + day;
 
     return [year, month, day].join('-');
+};
+function startDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + (d.getDate() - 2),//Start 2 from today. The user must select in the future
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+};
+
+function getDate() {
+
+    if (!selected) //http://stackoverflow.com/questions/5515310/is-there-a-standard-function-to-check-for-null-undefined-or-blank-variables-in
+    {
+        alert("Please choose a future date");
+        return false;
+    }
+    else {
+        document.getElementById('day').value = selected;
+        return true;
+    }
+
+
 };
